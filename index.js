298a528a82f2a0906b153539aa67511124d2f316
Danstan Oduor <danstan.oduor@student.moringaschool.com>
2022-08-25 17:17:05 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  // gives customers a free sample if the ride is less than or equal to 400 feet
+  if (distance<=400){
+  return ('This one is on me!')}
+ 
+  else if (distance>2000 && distance<=2500){
+
+   return( 'I will gladly take your thirty bucks.')
+  }
+  else {
+    return('No can do.');
+
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity( city ){
+  return (city==='NYC'? 'Ok, sounds good.':'No go.');
+  
+  // Write your code here!returns "Ok, sounds good."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous': 
+  return ("Thank you so much.");
+  break;
+  case'not as generous' : 
+  return ('Thank you.');
+  break;
+  default: 
+   return ('Bye.')
+  break;
+
+  }
   // Write your code here!
 }
